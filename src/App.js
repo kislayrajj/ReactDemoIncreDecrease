caf77e13c48eb6b4ad50168319e8286a1cafc43b
Kislay Raj <kislayraj751@gmail.com>
2024-02-23 20:34:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+  const incNum = () => {
+    setNum(num + 1);
+    document.querySelector("h1").style.color = "green";
+  };
+
+  const decNum = () => {
+    if (num > 0) {
+      setNum(num - 1);
+      document.querySelector("h1").style.color = "red";
+    } else {
+      setNum(0);
+      alert("Can not be less than 0");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div ">
+        <div className="center_div">
+          <h1> {num}</h1>
+          <div className="btn_div">
+            <button
+              onClick={incNum}
+              className="border-2 border-red-500 border-solid p-1"
+            >
+              Increase
+            </button>
+            <button onClick={decNum} className="border-2 border-red-500 border-solid p-1">Decrease</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
